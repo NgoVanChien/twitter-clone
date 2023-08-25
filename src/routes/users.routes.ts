946ecad10d0ef6404ff1e7a4c57750eff166c399
Ngo Van Chien <chienngo03568@gmail.com>
2023-08-26 00:08:22 +0700

    Error Handler trong Express.js

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -15,6 +15,26 @@ usersRouter.post('/login', loginValidator, loginController)
  * Method: POST
  * Body: { name: string, email: string, password: string, confirm_password: string, date_of_birth: ISO8601 }
  */
-usersRouter.post('/register', registerValidator, registerController)
+usersRouter.post(
+  '/register',
+  registerValidator,
+  async (req, res, next) => {
+    console.log('request handler 1')
+    // next(new Error('Lỗi rồi bạn êi'))
+    Promise.reject(new Error('Lỗi rồi bạn êi')).catch(next)
+  },
+  (req, res, next) => {
+    console.log('request handler 2')
+    next()
+  },
+  (req, res, next) => {
+    console.log('request handler 3')
+    res.json({ message: 'Register success' })
+  },
+  (err, req, res, next) => {
+    console.log('Lỗi là', err.message)
+    res.status(400).json({ error: err.message })
+  }
+)
 
 export default usersRouter
