443bc08e009e351ab5b57704d834df661f0ba474
Ngo Van Chien <chienngo03568@gmail.com>
2023-09-24 14:56:14 +0700

    fix bug add refresh token vào trong collection refresh_tokens

--- src/services/users.services.ts
+++ src/services/users.services.ts
@@ -143,6 +143,9 @@ class UsersService {
       ])
     ])
     const [access_token, refresh_token] = token
+    await databaseService.refreshTokens.insertOne(
+      new RefreshToken({ user_id: new ObjectId(user_id), token: refresh_token })
+    )
     return {
       access_token,
       refresh_token
