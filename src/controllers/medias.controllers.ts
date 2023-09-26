9d4475e234a4036c58fbefc1873fd206e388af70
ngovanchien <chienngo03568@gmail.com>
2023-09-27 00:04:03 +0700

    Fix bug header Content-Range không play được video

--- src/controllers/medias.controllers.ts
+++ src/controllers/medias.controllers.ts
@@ -44,16 +44,36 @@ export const serveVideoStreamController = (req: Request, res: Response, next: Ne
   // Dung lượng video (bytes)
   const videoSize = fs.statSync(videoPath).size
   // DUng lượng video cho mỗi phân đoạn stream
-  const chunkSize = 10 ** 6 // 1MB
+  const chunkSize = 30 * 10 ** 6 // 30MB
   // Lấy giá trị byte bắt đầu từ header Range (vd: bytes=1048576-)
   const start = Number(range.replace(/\D/g, ''))
-  // Lấy giá trị byte kết thúc, vượt quá dung lượng video thì lấy giá trị videoSize
-  const end = Math.min(start + chunkSize, videoSize)
+  // Lấy giá trị byte kết thúc, vượt quá dung lượng video thì lấy giá trị videoSize - 1
+  const end = Math.min(start + chunkSize, videoSize - 1)
 
   // Dung lượng thực tế cho mỗi đoạn video stream
   // THường đây sẽ là chunkSize, ngoại trừ đoạn cuối cùng
-  const contentLength = end - start
+  const contentLength = end - start + 1
   const contentType = mime.getType(videoPath) || 'video/*'
+
+  /**
+   * Format của header Content-Range: bytes <start>-<end>/<videoSize>
+   * Ví dụ: Content-Range: bytes 1048576-3145727/3145728
+   * Yêu cầu là `end` phải luôn luôn nhỏ hơn `videoSize`
+   * ❌ 'Content-Range': 'bytes 0-100/100'
+   * ✅ 'Content-Range': 'bytes 0-99/100'
+   *
+   * Còn Content-Length sẽ là end - start + 1. Đại diện cho khoản cách.
+   * Để dễ hình dung, mọi người tưởng tượng từ số 0 đến số 10 thì ta có 11 số.
+   * byte cũng tương tự, nếu start = 0, end = 10 thì ta có 11 byte.
+   * Công thức là end - start + 1
+   *
+   * ChunkSize = 50
+   * videoSize = 100
+   * |0----------------50|51----------------99|100 (end)
+   * stream 1: start = 0, end = 50, contentLength = 51
+   * stream 2: start = 51, end = 99, contentLength = 49
+   */
+
   const headers = {
     'Content-Range': `bytes ${start}-${end}/${videoSize}`,
     'Accept-Ranges': 'bytes',
