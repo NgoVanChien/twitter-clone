3099cff0fe8078ce75daf056aa1a4e1b46020165
ngovanchien <chienngo03568@gmail.com>
2023-09-30 16:37:00 +0700

    Tối ưu index khi khởi động server

--- src/services/database.services.ts
+++ src/services/database.services.ts
@@ -27,26 +27,45 @@ class DatabaseService {
     }
   }
 
-  indexUsers() {
-    this.users.createIndex({ email: 1, password: 1 })
-    this.users.createIndex({ email: 1 }, { unique: true })
-    this.users.createIndex({ username: 1 }, { unique: true })
+  // Create many indexs when start server, connected to database
+  async indexUsers() {
+    const exists = await this.users.indexExists(['email_1_password_1', 'email_1', 'username_1'])
+    // console.log(exists)
+    if (!exists) {
+      // Create an index on the email field, password field, username field
+      this.users.createIndex({ email: 1, password: 1 })
+      this.users.createIndex({ email: 1 }, { unique: true })
+      this.users.createIndex({ username: 1 }, { unique: true })
+    }
   }
 
-  indexRefreshTokens() {
-    this.refreshTokens.createIndex({ token: 1 })
-    this.refreshTokens.createIndex(
-      { exp: 1 },
-      {
-        expireAfterSeconds: 0
-      }
-    )
+  async indexRefreshTokens() {
+    const exists = await this.refreshTokens.indexExists(['exp_1', 'token_1'])
+
+    if (!exists) {
+      this.refreshTokens.createIndex({ token: 1 })
+      this.refreshTokens.createIndex(
+        { exp: 1 },
+        {
+          expireAfterSeconds: 0
+        }
+      )
+    }
   }
-  indexVideoStatus() {
-    this.videoStatus.createIndex({ name: 1 })
+
+  async indexVideoStatus() {
+    const exists = await this.videoStatus.indexExists(['name_1'])
+
+    if (!exists) {
+      this.videoStatus.createIndex({ name: 1 })
+    }
   }
-  indexFollowers() {
-    this.followers.createIndex({ user_id: 1, followed_user_id: 1 })
+
+  async indexFollowers() {
+    const exists = await this.followers.indexExists(['user_id_1_followed_user_id_1'])
+    if (!exists) {
+      this.followers.createIndex({ user_id: 1, followed_user_id: 1 })
+    }
   }
   get users(): Collection<User> {
     console.log(process.env.DB_USERS_COLLECTION)
