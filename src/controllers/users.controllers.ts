0b1bf08d0d7c8bc63ad86ec640ab4f4d5bfa2e8d
ngovanchien <chienngo03568@gmail.com>
2023-10-23 19:55:53 +0700

    Fake data bằng faker

--- src/controllers/users.controllers.ts
+++ src/controllers/users.controllers.ts
@@ -198,7 +198,7 @@ export const updateMeController = async (
 ) => {
   const { user_id } = req.decoded_authorization as TokenPayload
   const { body } = req
-  console.log(body)
+  // console.log(body)
   const user = await usersService.updateMe(user_id, body)
   return res.json({
     message: USERS_MESSAGES.UPDATE_ME_SUCCESS,
