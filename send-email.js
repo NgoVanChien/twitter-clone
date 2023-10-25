f15ce229136ddd37c6d4ed73f461572b89d0d909
ngovanchien <chienngo03568@gmail.com>
2023-10-25 22:01:59 +0700

    Gửi mail bằng AWS SES

---
+++ send-email.js
@@ -0,0 +1,69 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable no-undef */
+const { SendEmailCommand, SESClient } = require('@aws-sdk/client-ses')
+const { config } = require('dotenv')
+
+config()
+// Create SES service object.
+const sesClient = new SESClient({
+  region: process.env.AWS_REGION,
+  credentials: {
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID
+  }
+})
+
+const createSendEmailCommand = ({
+  fromAddress,
+  toAddresses,
+  ccAddresses = [],
+  body,
+  subject,
+  replyToAddresses = []
+}) => {
+  return new SendEmailCommand({
+    Destination: {
+      /* required */
+      CcAddresses: ccAddresses instanceof Array ? ccAddresses : [ccAddresses],
+      ToAddresses: toAddresses instanceof Array ? toAddresses : [toAddresses]
+    },
+    Message: {
+      /* required */
+      Body: {
+        /* required */
+        Html: {
+          Charset: 'UTF-8',
+          Data: body
+        }
+      },
+      Subject: {
+        Charset: 'UTF-8',
+        Data: subject
+      }
+    },
+    Source: fromAddress,
+    ReplyToAddresses: replyToAddresses instanceof Array ? replyToAddresses : [replyToAddresses]
+  })
+}
+
+const sendVerifyEmail = async (toAddress, subject, body) => {
+  const sendEmailCommand = createSendEmailCommand({
+    fromAddress: process.env.SES_FROM_ADDRESS,
+    toAddresses: toAddress,
+    body,
+    subject
+  })
+
+  try {
+    return await sesClient.send(sendEmailCommand)
+  } catch (e) {
+    console.error('Failed to send email.')
+    return e
+  }
+}
+
+sendVerifyEmail(
+  'chienngo03568@gmail.com',
+  'Tiêu đề email',
+  '<h1>Nội dung email được gửi từ nvchien.dev@gmail.com ngày 25</h1>'
+)
