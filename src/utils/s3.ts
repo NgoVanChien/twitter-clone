95980ea07233831026c092e5366d4e475cb0f85b
ngovanchien <chienngo03568@gmail.com>
2023-10-27 00:37:23 +0700

    Upload file ảnh lên aws s3

--- src/utils/s3.ts
+++ src/utils/s3.ts
@@ -1,4 +1,7 @@
 import { S3 } from '@aws-sdk/client-s3'
+import { Upload } from '@aws-sdk/lib-storage'
+import fs from 'fs'
+import path from 'path'
 import { config } from 'dotenv'
 config()
 const s3 = new S3({
@@ -8,4 +11,26 @@ const s3 = new S3({
     accessKeyId: process.env.AWS_ACCESS_KEY_ID as string
   }
 })
-s3.listBuckets({}).then((data) => console.log(data))
+// s3.listBuckets({}).then((data) => console.log(data))
+
+// D:\Projects\twitter-clone\uploads\images\landmark81.jpg
+
+const file = fs.readFileSync(path.resolve('uploads/images/landmark81.jpg'))
+const parallelUploads3 = new Upload({
+  client: s3,
+  params: { Bucket: 'twitter-clone-ap-southeast-1', Key: 'landmark.jpg', Body: file, ContentType: 'image/jpeg' },
+  tags: [
+    /*...*/
+  ], // optional tags
+  queueSize: 4, // optional concurrency configuration
+  partSize: 1024 * 1024 * 5, // optional size of each part, in bytes, at least 5MB
+  leavePartsOnError: false // optional manually handle dropped parts
+})
+
+parallelUploads3.on('httpUploadProgress', (progress) => {
+  console.log(progress)
+})
+
+parallelUploads3.done().then((res) => {
+  console.log(res)
+})
