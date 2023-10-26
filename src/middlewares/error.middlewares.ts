dc6f52e911eca60293ea2b252d8dcbd913406424
ngovanchien <chienngo03568@gmail.com>
2023-10-26 21:54:23 +0700

    Fix bug liên quan đến quyền truy cập các dịch vụ AWS

--- src/middlewares/error.middlewares.ts
+++ src/middlewares/error.middlewares.ts
@@ -4,14 +4,28 @@ import HTTP_STATUS from '~/constants/httpStatus'
 import { ErrorWithStatus } from '~/models/Errors'
 
 export const defaultErrorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
-  if (err instanceof ErrorWithStatus) {
-    return res.status(err.status).json(omit(err, ['status']))
+  try {
+    if (err instanceof ErrorWithStatus) {
+      return res.status(err.status).json(omit(err, ['status']))
+    }
+    const finalError: any = {}
+    Object.getOwnPropertyNames(err).forEach((key) => {
+      if (
+        !Object.getOwnPropertyDescriptor(err, key)?.configurable ||
+        !Object.getOwnPropertyDescriptor(err, key)?.writable
+      ) {
+        return
+      }
+      finalError[key] = err[key]
+    })
+    res.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).json({
+      message: finalError.message,
+      errorInfo: omit(finalError, ['stack'])
+    })
+  } catch (error) {
+    res.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).json({
+      message: 'Internal server error',
+      errorInfo: omit(error as any, ['stack'])
+    })
   }
-  Object.getOwnPropertyNames(err).forEach((key) => {
-    Object.defineProperty(err, key, { enumerable: true })
-  })
-  res.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).json({
-    message: err.message,
-    errorInfo: omit(err, ['stack'])
-  })
 }
