2d8546c7852a6cbe35c7c6db698bcc0a482ea6b8
ngovanchien <chienngo03568@gmail.com>
2023-10-26 16:48:39 +0700

    Code trang xác thực email bên client React

--- src/utils/email.ts
+++ src/utils/email.ts
@@ -80,7 +80,7 @@ export const sendVerifyRegisterEmail = (
       .replace('{{title}}', 'Please verify your email')
       .replace('{{content}}', 'Click the button below to verify your email')
       .replace('{{titleLink}}', 'Verify')
-      .replace('{{link}}', `${process.env.CLIENT_URL}/verify-email?token=${email_verify_token}`)
+      .replace('{{link}}', `${process.env.CLIENT_URL}/email-verifications?token=${email_verify_token}`)
   )
 }
 
