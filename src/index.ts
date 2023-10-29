11f7572a1e8150556eeaf02e285748b5015fbd4d
ngovanchien <chienngo03568@gmail.com>
2023-10-29 14:25:03 +0700

    Nhắn tin qua lại giữa 2 người

--- src/index.ts
+++ src/index.ts
@@ -60,7 +60,10 @@ io.on('connection', (socket) => {
   }
   console.log(users)
   socket.on('private message', (data) => {
-    const receiver_socket_id = users[data.to].socket_id
+    const receiver_socket_id = users[data.to]?.socket_id
+    if (!receiver_socket_id) {
+      return
+    }
     socket.to(receiver_socket_id).emit('receive private message', {
       content: data.content,
       from: user_id
