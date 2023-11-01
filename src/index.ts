2beab29d38581f1b36cbd4317303850bcf344a3a
ngovanchien <chienngo03568@gmail.com>
2023-11-01 16:52:58 +0700

    Giới hạn số lượng gọi request với rate limit

--- src/index.ts
+++ src/index.ts
@@ -21,6 +21,7 @@ import swaggerUi from 'swagger-ui-express'
 import swaggerJsdoc from 'swagger-jsdoc'
 import { envConfig, isProduction } from '~/constants/config'
 import helmet from 'helmet'
+import rateLimit from 'express-rate-limit'
 const file = fs.readFileSync(path.resolve('twitter-swagger.yaml'), 'utf8')
 const swaggerDocument = YAML.parse(file)
 
@@ -44,6 +45,14 @@ databaseService.connect().then(() => {
   databaseService.indexTweets()
 })
 const app = express()
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false // Disable the `X-RateLimit-*` headers
+  // store: ... , // Use an external store for more precise rate limiting
+})
+app.use(limiter)
 const httpServer = createServer(app)
 app.use(helmet())
 const corsOptions: CorsOptions = {
